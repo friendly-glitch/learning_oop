b60fad223b7e7d6f237a996dcbd67aa3a4dfcfec
friendly-glitch <cry3far3@gmail.com>
2025-01-26 23:38:27 +0500

    lesson16

--- src/public/index.php
+++ src/public/index.php
@@ -4,4 +4,5 @@ require_once '../vendor/autoload.php';
 
 //require_once '../app/L13/index.php';
 //require_once '../app/L14/index.php';
-require_once '../app/L15/index.php';
+//require_once '../app/L15/index.php';
+require_once '../app/L16/index.php';
